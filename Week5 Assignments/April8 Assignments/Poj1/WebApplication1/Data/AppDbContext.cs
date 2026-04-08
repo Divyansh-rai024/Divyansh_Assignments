c303e26e3b08f31c5b49fc55dcec64417775f33d
divyansh <divyansh200224@gmail.com>
2026-04-08 14:20:54 +0530

    Weekly Assignments

---
+++ Week5 Assignments/April8 Assignments/Poj1/WebApplication1/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+using System;
+
+namespace WebApplication1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Person> Person { get; set; } = null!;
+    }
+}
---
+++ Week5 Assignments/April8 Assignments/poj2/WebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public ProductsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.ApiBaseUrl = _config["ApiSettings:BaseUrl"];
+            return View();
+        }
+
+
+    }
+}
